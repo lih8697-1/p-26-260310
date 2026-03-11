75f63c7eaf5af7889fa41ccb7eb1381815a1b8c3
lih8697-1 <lih8697@gmail.com>
2026-03-11 11:36:05 +0900

    15

---
+++ src/main/java/com/back/domain/post/comment/controller/ApiV1CommentController.java
@@ -0,0 +1,67 @@
+package com.back.domain.post.comment.controller;
+
+import com.back.domain.post.comment.dto.CommentDto;
+import com.back.domain.post.comment.entity.Comment;
+import com.back.domain.post.post.entity.Post;
+import com.back.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts/{postId}/comments")
+public class ApiV1CommentController {
+
+    private final PostService postService;
+
+    @GetMapping
+    @ResponseBody
+    public List<CommentDto> list(
+            @PathVariable int postId
+    ) {
+        Post post = postService.findById(postId).get();
+        List<Comment> comments = post.getComments();
+
+        List<CommentDto> commentDtoList = comments.stream()
+                .map(CommentDto::new)
+                .toList();
+
+        return commentDtoList;
+    }
+
+    @GetMapping("/{commentId}")
+    @ResponseBody
+    public CommentDto detail(@PathVariable int postId, @PathVariable int commentId) {
+        Post post = postService.findById(postId).get();
+        Comment comment = post.findCommentById(commentId).get();
+
+        return new CommentDto(comment);
+    }
+
+    // /api/v1/posts/1/comments/1
+    @GetMapping("/{commentId}/delete")
+    @ResponseBody
+    @Transactional
+    public Map<String, Object> delete(
+            @PathVariable int postId,
+            @PathVariable int commentId
+    ) {
+        Post post = postService.findById(postId).get();
+        post.deleteComment(commentId);
+
+        Map<String, Object> result = Map.of(
+                "msg", "%d번 댓글이 삭제됐습니다.".formatted(commentId),
+                "resultCode", "204-1"
+        );
+
+        return result;
+    }
+}
